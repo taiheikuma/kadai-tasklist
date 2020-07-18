b49c5bfd8ade351c6c1d5e260427f82bd043a9a0
taihei.kuma <tai.kuma@gmail.com>
2020-07-18 07:30:50 +0000

    users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 root to: 'toppages#index'
 
+get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+
 root to: 'tasks#index'
 
   resources :tasks
